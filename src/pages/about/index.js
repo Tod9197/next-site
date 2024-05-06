0f21346bca246710c640f8cf82f6dff0de429a84
Tod9197 <dancein18@yahoo.co.jp>
2024-05-06 09:27:02 +0900

    First Commit

---
+++ src/pages/about/index.js
@@ -0,0 +1,10 @@
+import Header from "../components/header";
+import Content from "../components/contents";
+
+export default function About() {
+  return (
+    <Content>
+      <Header title="About" />
+    </Content>
+  );
+}
